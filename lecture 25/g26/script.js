822b5c8312900232ba49d7289c606d68aad29b8b
Jitendra Singh <33808010+Bahubuli@users.noreply.github.com>
2023-05-16 16:11:24 +0530

    intro to jquery

---
+++ lecture 25/g26/script.js
@@ -0,0 +1,60 @@
+// let h1El = $("h1")
+// jQuery object
+// console.log($("h1"))
+
+// .css()
+// $("h1").css("background-color","aqua")
+
+// jQuery()
+
+// let myStyle = {
+//     border:"2px solid blue",
+//     fontSize:"40px",
+//     backgroundColor:"red"
+// }
+
+// $('h1').css(myStyle);
+
+// .text()
+// console.log($("p").text())
+// $("p").text("this is new text")
+
+// .html()
+// console.log($("p").html())
+// $("p").html("this <strong>is</strong> new html text")
+
+// .attr()
+// let ip = $("input[type='text']")
+// console.log(ip.attr("type"))
+
+// ip.attr("type","radio")
+
+//addClass, removeClass, toggleClass
+// classList.add(), classList.remove()
+// $("p").addClass("red")
+// $("p").removeClass("red")
+
+// $("p").toggleClass("red");
+// $("p").toggleClass("red");
+
+
+// first(), last()
+
+// $("li").last().css("color","red")
+
+// // $("li:nth-child(2)")
+
+// console.log($($("li")[1]).css("color","blue"))
+
+// append prepend
+// $("ul").prepend($("<li>aquaman</li>"))
+
+
+// // before after
+// $("#batman").after($("<li>spiderman</li>"))
+
+
+$(document).ready(
+
+    console.log("document is ready")
+)
